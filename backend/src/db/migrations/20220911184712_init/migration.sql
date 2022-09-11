e6a268c6ece6443f250f655e814c6a52c35344bd
Nusendra Hanggarawan <admin@nusendra.com>
2022-09-12 01:48:20 +0700

    feat: add user database migration

---
+++ backend/src/db/migrations/20220911184712_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
